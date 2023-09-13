82fc92b3ca0513169dc1f8d7b6754af5d1440052
gastonhceb <gastonhceb@gmail.com>
2023-09-13 20:01:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+  },
+
+  {
+    id: 3,
+    nombre: "pizza Napolitana",
+    precio: 1350,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+  },
+
+  {
+    id: 4,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+  },
+
+  {
+    id: 5,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+  },
+
+  {
+    id: 6,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+  },
+];
+
+// a)  Las pizzas que tengan un id impar.
+
+pizzas.forEach((pizzas) => {
+  if (pizzas.id % 2 === 1) {
+    console.log(
+      `La pizza: ${pizzas.id} tiene un número de identificación impar`
+    );
+  }
+});
+
+//b) Responder: ¿Hay alguna pizza que valga menos de $600?
+
+let PizzaDeValorA_ = false;
+pizzas.forEach((pizza) => {
+  if (pizza.precio < 600) {
+    PizzaDeValorA_ = true;
+  }
+});
+if (PizzaDeValorA_) {
+  console.log(`Sí, tenemos pizzas que cuestan menos de $600.`);
+}
+
+//c) El nombre de cada pizza con su respectivo precio.
+
+pizzas.forEach((pizzas) => {
+  const { nombre, precio } = pizzas;
+  console.log(`La ${nombre}, tiene un valor de $${precio}`);
+});
+
+//d) Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo). Ayuda: van a tener que realizar dos recorridos, ya que cada pizza del array de pizzas tiene una propiedad "ingredientes" cuyo valor es un array con ingredientes.
+
+pizzas.forEach((pizza) => {
+  console.log(`Ingredientes de la ${pizza.nombre}:`);
+  pizza.ingredientes.forEach((ingrediente) => {
+    console.log(ingrediente);
+  });
+  console.log("");
+});
